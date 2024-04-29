936648542896c07232932323cc524293b567a9e8
Evgeny Ukhanov <evgeny@aurora.dev>
2024-04-30 00:53:41 +0200

    Added file-page splitter

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 const PAGE_SIZE: u64 = 2700; // 8200 for 12px
-const START_HTML: &str = r#"<html><body><div style="font-family: verdana;font-size: 60px; font-weight: bold; text-align: justify">"#;
+const START_HTML: &str = r#"<html><head><meta charset="UTF-8"><.head><body><div style="font-family: verdana;font-size: 60px; font-weight: bold; text-align: left">"#;
 const END_HTML: &str = r#"</div></body></html>"#;
 
 fn main() -> std::io::Result<()> {
@@ -10,6 +10,17 @@ fn main() -> std::io::Result<()> {
         return Ok(());
     }
     let data = std::fs::read_to_string(&a[1])?;
+    let pages_per_file = if a.len() >= 3 {
+        a[2].parse::<u64>()
+            .expect("expected page size per file as integer")
+    } else {
+        0
+    };
+    let result_file = if a.len() == 4 {
+        a[3].clone()
+    } else {
+        "index".to_string()
+    };
 
     let mut html: Vec<String> = vec![START_HTML.to_string()];
     let mut page_counter: u64 = 0;
@@ -39,6 +50,16 @@ fn main() -> std::io::Result<()> {
                     .join(""),
                 );
                 page_words = vec![word];
+                if pages_per_file > 0 {
+                    if page % pages_per_file == 0 {
+                        let file_index = page / pages_per_file;
+                        let file_name = format!("{result_file}{file_index}.html");
+                        html.push(END_HTML.to_string());
+                        std::fs::write(file_name, html.join("\n")).unwrap();
+                        // Re-init html
+                        html = vec![START_HTML.to_string()];
+                    }
+                }
             }
         }
     }
